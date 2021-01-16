51ace4a75e8815940e2f30b4e75cf8db77343e75
hannanel100 <hannanel100@gmail.com>
2021-01-16 23:56:14 +0200

    upto 2:28:11

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Telegram from './components/Telegram'
+import Login from './components/Login'
+import {selectUser, login, logout} from './features/userSlice'
+
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {auth} from './firebase'
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if(authUser){
+        //login
+        dispatch(login({
+          uid: authUser.uid,
+          photo: authUser.photoURL,
+          email: authUser.email,
+          displayName: authUser.displayName
+        }))
+      }
+      else{
+        dispatch(logout())}
+    })
+    return () => {
+      
+    }
+  }, [dispatch])
   return (
     <div className="app">
-      <Telegram />
+      {user ? <Telegram /> : <Login />}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,18 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBjzvTsQo2qQmpsW8GekQgfffqopDa3YRM",
-    authDomain: "telegram-clone-7c83b.firebaseapp.com",
-    projectId: "telegram-clone-7c83b",
-    storageBucket: "telegram-clone-7c83b.appspot.com",
-    messagingSenderId: "855148582226",
-    appId: "1:855148582226:web:ebdbdadde2f305791b568b"
-  };
+  apiKey: "AIzaSyBjzvTsQo2qQmpsW8GekQgfffqopDa3YRM",
+  authDomain: "telegram-clone-7c83b.firebaseapp.com",
+  projectId: "telegram-clone-7c83b",
+  storageBucket: "telegram-clone-7c83b.appspot.com",
+  messagingSenderId: "855148582226",
+  appId: "1:855148582226:web:ebdbdadde2f305791b568b",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
